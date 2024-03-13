f7c0c6dfd1dd8e9c8c6d496d7e8e6de3a24f0de2
Salomao001 <eduardo.salomao.584@gmail.com>
2024-03-12 21:25:31 -0300

    atividade 1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,23 @@
+const aSoma = 10;
+const bSoma = 15;
+
+const aSub = 20;
+const bSub = 10;
+
+const aMulti = 5;
+const bMulti = 5;
+
+const aDiv = 25;
+const bDiv = 5;
+
+const soma = aSoma + bSoma;
+const sub = aSub - bSub;
+const multi = aMulti * bMulti;
+const div = aDiv / bDiv;
+
+console.log(`Resultado da soma entre ${aSoma} e ${bSoma} = ${soma}`);
+console.log(`Resultado da subtração entre ${aSub} e ${bSub} = ${sub}`);
+console.log(
+  `Resultado da multiplicação entre ${aMulti} e ${bMulti} = ${multi}`
+);
+console.log(`Resultado da divisão entre ${aDiv} e ${bDiv} = ${div}`);
